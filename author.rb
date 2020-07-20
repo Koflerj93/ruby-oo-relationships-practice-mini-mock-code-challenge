44ecedea8b9a8be613a3a40142a4331327899d39
github username <github email address>
2020-07-20 13:52:49 -0400

    done

--- author.rb
+++ author.rb
@@ -1,1 +1,44 @@
+require 'pry'
+class Author 
+
+    attr_accessor :name 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def books 
+        Book.all.select do |book|
+            book.author = self 
+        end 
+    end 
+
+    def write_book(title, word_count)
+        book = Book.new(title, word_count)
+        book.author = self 
+    end 
+
+    def total_words
+        word_counts = books.sum do |book|
+            book.word_count
+        end
+        word_counts
+    end
+
+    def self.most_words
+        most_words = 0
+        books.each do |book| 
+            if book.word_count > most_words
+                most_words = book.word_count
+            end 
+        end
+        most_words 
+    end 
+end 
 
--- console.rb
+++ console.rb
@@ -2,6 +2,13 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+
+book1 = Book.new("Carl", "with hats", 5)
+book2 = Book.new("bob", "Real estate for real", 10)
+book3 = Book.new("bob", "how to eat apples", 45)
+a1 = Author.new("Carl")
+a2 = Author.new("bob")
 binding.pry
+
 
 
